f580dc25be4c5abbfa695cfab76eb00999edb2f7
Hynek Cernoch <hynek@sdb.cz>
2011-03-19 22:38:22 +0100

    Enhanced installation diagnostics by satchmo_check. see #1284

--- satchmo/apps/satchmo_store/shop/management/commands/satchmo_check.py
+++ satchmo/apps/satchmo_store/shop/management/commands/satchmo_check.py
@@ -1,15 +1,20 @@
+from decimal import Decimal
+from distutils.version import LooseVersion
 from django.core.management.base import NoArgsCommand
 from django.core import urlresolvers
 from django.utils.importlib import import_module
+import django
+import imp
 import logging
 import re
 import sys
-import django
-from decimal import Decimal
 import types
-from distutils.version import LooseVersion
 
 errors = []
+logged_more = False
+app_paths = {}
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger('satchmo_check').setLevel(logging.DEBUG)
 log = logging.getLogger('satchmo_check')
 
 class Command(NoArgsCommand):
@@ -26,21 +31,25 @@ class Command(NoArgsCommand):
         If was seen a traceback, it should be reported to a developer. (for now)
         Tracebacks are saved to the 'satchmo.log'. It helps to find cyclic dependencies etc.
         """
-        #    print_out(...)  # print immediately and log it
-        #    error_out(...)  # print at the end and log it now
         from django.conf import settings
+        global logged_more
         print_out("Checking your satchmo configuration.")
         try:
             import satchmo_store
         except ImportError:
             error_out("Satchmo is not installed correctly. Please verify satchmo is on your sys path.")
-        print_out("Using Django version %s" % django.get_version())
-        print_out("Using Satchmo version %s" % satchmo_store.get_version())
+        print "Using Django version %s" % django.get_version()
+        print "Using Satchmo version %s" % satchmo_store.get_version()
         #Check the Django version
         #Make sure we only get the X.Y.Z version info and not any other alpha or beta designations
         version_check = LooseVersion(".".join(map(str, django.VERSION)[:3]))
         if version_check < LooseVersion("1.2.3"):
             error_out("Django version must be >= 1.2.3")
+
+        # Store these checked installation paths also to the paths overview
+        verbose_check_install('satchmo', 'satchmo_store', verbose_name='Satchmo')
+
+        verbose_check_install('django', 'django', '1.2.3')
 
         # Try importing all our dependencies
         verbose_check_install('', 'Crypto.Cipher', verbose_name='The Python Cryptography Toolkit')
@@ -62,13 +71,18 @@ class Command(NoArgsCommand):
 
         verbose_check_install('django_caching_app_plugins', 'app_plugins', '0.1.2', '53a31761e344')
 
-        verbose_check_install('django_livesettings', 'livesettings', '1.4.8', 'e2769f9f60ec')
+        verbose_check_install('django_livesettings', 'livesettings', '1.4-8', 'e2769f9f60ec')
 
         verbose_check_install('django_signals_ahoy', 'signals_ahoy', '0.1.0', '9ad8779d4c63')
 
         verbose_check_install('django_threaded_multihost', 'threaded_multihost', '1.4.1', '7ca3743d8a70')
 
-        verbose_check_install('django-keyedcache', 'keyedcache', '1.4.4', '4be18235b372')
+        verbose_check_install('django-keyedcache', 'keyedcache', '1.4-4', '4be18235b372')
+
+        # Installers versions can be interesting for installation problems
+        check_install('pip', 'pip')   # pip can not show the version number
+        verbose_check_install('setuptools', 'setuptools', required=False)
+        #verbose_check_install('mercurial', 'mercurial', required=False)
 
         try:
             cache_avail = settings.CACHE_BACKEND
@@ -79,7 +93,7 @@ class Command(NoArgsCommand):
             url = urlresolvers.reverse('satchmo_search')
             # Catch SystemExit, because if an error occurs, `urlresolvers` usually calls sys.exit() and other error messages would be lost.
         except (Exception, SystemExit), e:
-            error_out("Unable to resolve url. Received error - %s" % e)
+            error_out("Unable to resolve urls. Received error - %s" % formaterror(e))
         from l10n.l10n_settings import get_l10n_default_currency_symbol
         if not isinstance(get_l10n_default_currency_symbol(),types.UnicodeType):
             error_out("Your currency symbol should be a unicode string.")
@@ -105,13 +119,28 @@ class Command(NoArgsCommand):
         # Check all installed apps
         if not filter(lambda x: re.search('not .*(installed|imported)', x), errors):
             for appname in settings.INSTALLED_APPS:
+                pkgtype, filename, root_filename = find_module_extend(appname)
                 try:
                     app = import_module(appname)
                 except (Exception, SystemExit):
-                    error_out('Can not import "%s"' % appname) 
-                    log.exception('Can not import "%s"' % appname)
+                    if not pkgtype:
+                        error_out('Can not find module "%s"' % appname)
+                    else:
+                        error_out('Can not import "%s"' % appname) 
+                        log.exception('Can not import "%s"' % appname)
+                        logged_more = True
         else:
             log.debug('It does not test INSTALLED_APPS due to previous errors.')
+
+        log.debug('\n'.join(2 * ['Installation paths:'] +
+                    ['  %s : %s' % (k, sorted(list(v))) for k, v in sorted(app_paths.items())]
+                ))
+        apps_in_root = sorted(reduce(set.union,
+                [v for k, v in app_paths.items() if k.startswith('/root')],
+                set()))
+        if apps_in_root:
+            error_out('No package should be installed in the "/root" home directory, but packages %s are.' % (apps_in_root,))
+            logged_more = True
 
         if len(errors) == 0:
             print_out("Your configuration has no errors.")
@@ -119,16 +148,53 @@ class Command(NoArgsCommand):
             print_out(""); print_out("The following errors were found:")
             for error in errors:
                 print_out(error)
-            if filter(lambda x: re.search('not .*import(ed)?', x), errors):
+            if logged_more:
                 print "Error details are in 'satchmo.log'"
 
 def print_out(msg):
+    "Print immediately to screen and to the log."
     log.info(msg)
     print msg
 
 def error_out(msg):
+    "Prints not to the log and at the end to the screen."
     log.error(msg)
     errors.append(msg)
+
+def formaterror(e):
+    "Format an exception like this: 'ExceptionName: error message'."
+    exc_name = getattr(type(e), '__name__', None)
+    if not exc_name:
+        # some exceptions defined in C++ do not have an attribute __name__
+        # e.g. backend.DatabaseError for postgres_psycopg2 
+        exc_name = re.sub(r"<.*'(.*)'>", '\\1', str(type(e)))
+        exc_name = exc_name.split('.')[-1]
+    return '%s: %s' % (exc_name, e)
+
+def find_module_extend(appname):
+    """Find module - support for "package.package.module".
+    Returns tuple (pkgtype, filename, root_filename)   (result constants are defined in imp)
+    root_filename is for the firstlevel package
+    This does not find an yet imported module which is not on the python path now and was only at the time of import (typically store.localsite)
+    """
+    lpos = 0
+    path = pkgtype = None
+    try:
+        while lpos < len(appname):
+            rpos = (appname.find('.', lpos) < 0) and len(appname) or appname.find('.', lpos)
+            dummyfile, filename, (suffix, mode, pkgtype) = imp.find_module(appname[lpos:rpos], path)
+            path = [filename]
+            if not lpos:
+                root_filename = filename
+            lpos = rpos + 1
+        root_appname = appname.split('.')[0]
+        if root_filename.endswith('/' + root_appname):
+            root_filename = root_filename[:-len(root_appname) - 1]
+        app_paths.setdefault(root_filename, set())
+        app_paths[root_filename].add(root_appname)
+        return (pkgtype, filename, root_filename)
+    except ImportError:
+        return (None, None, None)
 
 APP_NOT_FOUND, APP_OLD_VERSION, APP_IMPORT_FAIL, APP_OK = range(4)
 
@@ -151,30 +217,23 @@ def check_install(project_name, appname, min_version=None, hg_hash=None):
     """
     # Version number can be obtained even without any requirements for a version this way: (all versions meet this)
     #    check_install(project_name, appname, min_version='.', hg_hash='')
-    import imp
     import os
     import time
-    isfound = isimported = False
-    pkgtype = isversion = None
+    global logged_more
+    isimported = False
+    isversion = None
     version = ''
-    try:
-        # find it only
-        lpos = 0
-        path = None
-        while lpos < len(appname):
-            rpos = (appname.find('.', lpos) < 0) and len(appname) or appname.find('.', lpos)
-            dummyfile, filename, (suffix, mode, pkgtype) = imp.find_module(appname[lpos:rpos], path)
-            path = [filename]
-            lpos = rpos + 1
-        isfound = True
+    # find it only
+    pkgtype, filename, root_filename = find_module_extend(appname)
+    isfound = (pkgtype != None)
+    if isfound:
         try:
             # import it
             app = __import__(appname)
             isimported = True
         except ImportError:
             log.exception('Can not import "%s"' % appname)
-    except ImportError:
-        pass
+            logged_more = True
 
     if isimported:
         try:
@@ -236,10 +295,16 @@ def check_install(project_name, appname, min_version=None, hg_hash=None):
     result = isfound and (isversion and (isimported and APP_OK or APP_IMPORT_FAIL) or APP_OLD_VERSION) or APP_NOT_FOUND
     return (result, version)
 
-def verbose_check_install(project_name, appname, min_version=None, hg_hash=None, verbose_name=None):
+def verbose_check_install(project_name, appname, min_version=None, hg_hash=None, verbose_name=None, required=True):
+    """Check a pagkage and writes the results.
+
+    Calls ``check_install`` (see for info about the similar parameters)
+    verbose_name is used for messageses. Default is same as appname."""
     result, version = check_install(project_name, appname, min_version, hg_hash)
     verbose_name = (verbose_name or re.sub('[_-]', ' ', appname.capitalize()))
-    log.debug('%s: found version %s ' % (verbose_name, version or '(unknown)'))
+    if result != APP_NOT_FOUND and required or result == APP_OK:
+        log.debug('%s: version %s ' % (verbose_name, version or '(unknown)'))
+    #
     if result == APP_NOT_FOUND:
         msg = 'is not installed.'
     elif result == APP_OLD_VERSION:
@@ -248,5 +313,6 @@ def verbose_check_install(project_name, appname, min_version=None, hg_hash=None,
         msg = 'can not be imported now, but the right version is probably installed. Maybe dependency problem.'
     elif result == APP_OK:
         msg = None
-    if msg:
+    #
+    if msg and required:
         error_out(' '.join((verbose_name, msg)))
