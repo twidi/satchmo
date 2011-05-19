7b15698a2cbecd6756d2dcae2ce13059bbf0d77d
twidi <s.angel@twidi.com>
2011-05-19 23:39:40 +0200

    In the "product_upsell" templatetag, return a RequestContext, not only a simple context, to provide some context var like "sale" to calculate the price of upsell products

--- satchmo/apps/satchmo_ext/upsell/templatetags/satchmo_upsell.py
+++ satchmo/apps/satchmo_ext/upsell/templatetags/satchmo_upsell.py
@@ -2,13 +2,13 @@ from django import template
 
 register = template.Library()
 
-def product_upsell(product):
+def product_upsell(context, product):
     """
     Display the list of products that are upsell candidates for currently viewed product.
     """
     goals = None
     if product.upselltargets.count() > 0:
         goals = product.upselltargets.all()
-        
-    return { 'goals' : goals }
-register.inclusion_tag("upsell/product_upsell.html", takes_context=False)(product_upsell)
+
+    return template.RequestContext(context['request'], { 'goals' : goals })
+register.inclusion_tag("upsell/product_upsell.html", takes_context=True)(product_upsell)
